b8e8d9fd9f32d43bca4bef1d8f9c0530dfc4d396
rfarcy <rfarcy@NEPS-PC13>
2023-07-05 15:42:36 -0300

    erreglo bug tipo 6

---
+++ BackEnd/API/Models/FormulariosRegistrados/FormDataSucursal.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PectraForms.WebApplication.BackEnd.API.Models.FormulariosRegistrados
+{
+    public class FormDataSucursal
+    {
+        public string _Id { get; set; }
+        public FieldsSucursal Fields { get; set; }
+    }
+    public class FieldsSucursal
+    {
+        public List<PageFormDataSucursal> Pages { get; set; }
+    }
+
+    public class PageFormDataSucursal
+    {
+        public List<ElementsSucursal> Elements { get; set; }
+    }
+
+    public class ElementsSucursal
+    {
+        public string FieldToLoad { get; set; }
+        public string Value { get; set; }
+    }
+
+}
